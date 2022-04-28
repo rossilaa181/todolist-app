6d2240e8aa1166d86b633e94d41b5c9ef7c398b3
rossilaa181 <latansasalsabela@gmail.com>
2022-04-28 13:16:55 +0700

    Praktikum 2 CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('/todos', TodoController::class);
 });
 
 
